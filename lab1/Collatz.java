0d5e3915d094b9ce6cfde23e944f186ceccbc5aa
Long Nguyen <long.nguyen-duy@outlook.com>
2023-02-28 10:21:19 +0700

    add lab1 collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,16 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author longiee
  */
 public class Collatz {
-
-    /** Buggy implementation of nextNumber! */
+    /** Function to calculate the next number in the Collatz sequence
+     * If the number is even, the next number if n/2
+     * If the number is odd, the next number is 3n+1
+     */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if ((n%2) == 0) {
+            return n/2;
         } else {
-            return n * 2;
+            return 3*n+1;
         }
     }
 
